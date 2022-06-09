8d491c647a569229db993501ff5bff07464b897e
TsaplinIA <bkmzfccfcby@gmail.com>
2022-06-09 23:30:35 +0300

    Add Database.py

---
+++ Database.py
@@ -0,0 +1,27 @@
+import Config
+from pathlib import Path
+from peewee import SqliteDatabase, Model, AutoField, TextField, BlobField
+
+db = SqliteDatabase(Config.DATABASE_FILENAME)
+exists = Path(Config.DATABASE_FILENAME).exists()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    id = AutoField(column_name="user_id")
+    name = TextField(column_name="name")
+    surname = TextField(column_name="surname")
+    email = TextField(column_name="email", unique=True)
+    eth_address = TextField(column_name="eth_address", unique=True)
+    password = BlobField(column_name="password")
+
+    class Meta:
+        table_name = "Users"
+
+
+if not exists:
+    User.create_table()
