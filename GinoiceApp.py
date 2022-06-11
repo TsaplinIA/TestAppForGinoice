93d9fad7b2f6ee8e8bfd903263cb9da803bf872d
TsaplinIA <bkmzfccfcby@gmail.com>
2022-06-11 16:05:26 +0300

    add logic

--- GinoiceApp.py
+++ GinoiceApp.py
@@ -1,13 +1,15 @@
 import json
 import re
 
-from flask import Flask, redirect
+from flask import Flask, redirect, request, make_response
 from flask_swagger_ui import get_swaggerui_blueprint
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_webframeworks.flask import FlaskPlugin
 
 from Models import *
+from MyTools import check_correct_request, register_new_user, my_login, get_user_info
+from TokenTools import check_token
 
 app = Flask(__name__)
 
@@ -41,7 +43,7 @@ app.template_folder = "./static"
 
 
 @app.route('/sign_up', methods=['POST'])
-def registration(**kwargs):
+def registration():
     """
     ---
     post:
@@ -59,7 +61,12 @@ def registration(**kwargs):
                     application/json:
                         schema: ErrorSchema
             401:
-                description: Wrong email or password
+                description: Invalid email or password
+                content:
+                    application/json:
+                        schema: ErrorSchema
+            410:
+                description: User already registered
                 content:
                     application/json:
                         schema: ErrorSchema
@@ -73,8 +80,21 @@ def registration(**kwargs):
                 application/json:
                     schema: RegisterRequestSchema
     """
+    request_json = request.json
+    match(check_correct_request(request_json, RegisterRequestSchema)):
+        case 400:
+            return make_response({"error": "Bad request"}, 400)
+        case 401:
+            return make_response({"error": "Invalid email or password"}, 401)
+        case _:
+            pass
 
-    return {"user_id": 1, "signature": "Some"}
+    register_json = register_new_user(request_json)
+    error_message = register_json.get("error", None)
+    if error_message:
+        return make_response({"error": error_message}, 410)
+
+    return make_response({"user_id": register_json["user_id"], "signature": register_json["user_sign"]}, 200)
 
 
 @app.route('/sign_in', methods=['POST'])
@@ -96,7 +116,7 @@ def login():
                     application/json:
                         schema: ErrorSchema
             401:
-                description: Wrong email or password
+                description: Invalid/Wrong email or password
                 content:
                     application/json:
                         schema: ErrorSchema
@@ -110,8 +130,18 @@ def login():
                 application/json:
                     schema: LoginRequestSchema
     """
-    pass
-    return "login"
+    request_json = request.json
+    match (check_correct_request(request_json, LoginRequestSchema)):
+        case 400:
+            return make_response({"error": "Bad request"}, 400)
+        case 401:
+            return make_response({"error": "Invalid email or password"}, 401)
+        case _:
+            pass
+    token = my_login(request_json["email"], request_json["password"])
+    if token:
+        return make_response({"auth_token": token}, 200)
+    return make_response({"error": "Wrong email or password"}, 401)
 
 
 @app.route('/user', methods=['GET'])
@@ -130,7 +160,12 @@ def user():
                     application/json:
                         schema: UserSchema
             401:
-                description: You should have logged in
+                description: You must have logged in
+                content:
+                    application/json:
+                        schema: ErrorSchema
+            403:
+                description: Invalid token
                 content:
                     application/json:
                         schema: ErrorSchema
@@ -140,8 +175,16 @@ def user():
                     application/json:
                         schema: ErrorSchema
     """
-    pass
-    return "user"
+    try:
+        auth_header = request.headers.get('Authorization')
+    except KeyError:
+        return make_response({"error": "You must have logged in"}, 401)
+    auth_token = re.sub(r"Bearer\s+", "", auth_header)
+    user_id = check_token(auth_token)
+    if user_id:
+        return make_response(get_user_info(user_id), 200)
+
+    return make_response({"message": "Invalid token"}, 403)
 
 
 with app.test_request_context():
@@ -160,6 +203,11 @@ def swagger():
     file = json.dumps(spec.to_dict(), indent=2)
     fixed_file = re.sub(r"{\s*\"bearerAuth\":\s*\[\]\s*\}", "[{\n\"bearerAuth\": []\n}]", file)
     return fixed_file
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response({"error": "Bad request"}, 400)
 
 
 if __name__ == "__main__":
