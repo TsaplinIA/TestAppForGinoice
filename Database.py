9ffd0a234e60dcc1559f33b8ccdb631760f670ee
TsaplinIA <bkmzfccfcby@gmail.com>
2022-06-11 16:05:45 +0300

    add new Column in database

--- Database.py
+++ Database.py
@@ -18,6 +18,7 @@ class User(BaseModel):
     email = TextField(column_name="email", unique=True)
     eth_address = TextField(column_name="eth_address", unique=True)
     password = BlobField(column_name="password")
+    auth_token = TextField(column_name="auth_token")
 
     class Meta:
         table_name = "Users"
